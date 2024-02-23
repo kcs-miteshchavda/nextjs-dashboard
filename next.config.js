c7530c8a50af81d896a3a5de47c1f252c0ac75df
kcs-miteshchavda <86779935+kcs-miteshchavda@users.noreply.github.com>
2024-02-23 18:18:33 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-  env: {
-    API_ENDPOINT: process.env.API_ENDPOINT,
-    APP_URL: process.env.APP_URL,
-  },
   experimental: {
     serverActions: {
       allowedOrigins: ['http://172.17.1.53:9027', 'http://localhost:9027'],
